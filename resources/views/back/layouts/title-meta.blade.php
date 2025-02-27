1bf5cccfe7e98658bb67633946bc2f20cadb81b5
suryo <suryoatm@gmail.com>
2025-02-27 08:13:39 +0700

    modified js

--- resources/views/back/layouts/title-meta.blade.php
+++ resources/views/back/layouts/title-meta.blade.php
@@ -24,3 +24,19 @@
 		<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
 		{{-- <link href="{{ URL::asset('/assets/libs/datatables/datatables.min.css') }}" rel="stylesheet" type="text/css" /> --}}
+
+        <link href="{{ url('/') }}/ui/assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet"
+    type="text/css" />
+
+<link href="{{ url('/') }}/ui/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet"
+    type="text/css" />
+
+
+<link href="{{ url('/') }}/ui/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+{{-- <link href="{{ url('/') }}/ui/assets/css/style.bundle.css" rel="stylesheet" type="text/css" /> --}}
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+
+<!-- Toast Tr -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
