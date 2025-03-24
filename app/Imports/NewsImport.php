6a92454f5356abf91b50aa8e217c863c62a8e3cb
suryo <suryoatm@gmail.com>
2025-03-24 10:13:05 +0700

    modified import news field

--- app/Imports/NewsImport.php
+++ app/Imports/NewsImport.php
@@ -11,10 +11,19 @@ class NewsImport implements ToModel, WithHeadingRow
     public function model(array $row)
     {
         return new News([
-            'title'       => $row['title'],
-            'content'     => $row['content'],
-            'category'    => $row['category'],
-            'published_at' => $row['published_at'],
+            // 'title'       => $row['title'],
+            // 'content'     => $row['content'],
+            // 'category'    => $row['category'],
+            // 'published_at' => $row['published_at'],
+            'title' => $row['title'],
+            'short_desc' => $row['short_desc'],
+            'content' => $row['content'],
+            'is_breaking_news' => $row['is_breaking_news'],
+            'author' => $row['author'],
+            'slug' => $row['slug'],
+            'status' => $row['status'],
+            'views' => $row['views'],
+
         ]);
     }
 }
