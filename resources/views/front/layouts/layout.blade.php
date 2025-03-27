50c415dc028d544d88ef8edb17a49a9b94d054c4
suryo <suryoatm@gmail.com>
2025-03-27 13:48:16 +0700

    add meta google

--- resources/views/front/layouts/layout.blade.php
+++ resources/views/front/layouts/layout.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="77JWxQbqV-H-A9qKsDqm75U3ZdugkZOSvHAYtlj2gYA" />
     <title>@yield('title', 'News Website')</title>
 
     <!-- Bootstrap CSS -->
