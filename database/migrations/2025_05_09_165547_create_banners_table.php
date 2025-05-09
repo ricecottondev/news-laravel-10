09cc08d3c5d81827ac7da7a8206afbedd9f7dc91
Danu-Nur <diramadhan13@gmail.com>
2025-05-09 15:57:31 +0700

    Create migration for banners table with necessary fields

---
+++ database/migrations/2025_05_09_165547_create_banners_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('image_desktop')->nullable();
+            $table->string('image_mobile')->nullable();
+            $table->string('start')->nullable();
+            $table->string('end')->nullable();
+            $table->string('status')->default('inactive');
+            $table->boolean('deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('banners');
+    }
+};
