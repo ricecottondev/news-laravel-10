fed9c01db8485744185f95a1203629431342b992
Danu-Nur <diramadhan13@gmail.com>
2025-05-24 09:57:42 +0700

    add middleware to detect bots and display custom access denied page

---
+++ resources/views/bot-detected.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Bot Detected</title>
+</head>
+
+<body style="font-family: sans-serif; text-align: center; margin-top: 100px;">
+    <h1>🤖 Access Denied</h1>
+    <p>We've detected that you're a robot or automated crawler. If you're not, please contact admin.</p>
+</body>
+
+</html>
