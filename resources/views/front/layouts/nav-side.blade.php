4b7ea4fe54fbd852a6512b41bcff92a1fb836be6
Suryo <suryoatm@gmail.com>
2025-05-28 21:15:16 +0700

    fixed url for nav-side

--- resources/views/front/layouts/nav-side.blade.php
+++ resources/views/front/layouts/nav-side.blade.php
@@ -1,31 +1,36 @@
 <ul class="nav flex-column text-capitalize row-gap-1">
    <li class="nav-item">
-      <a href="#" class="nav-link text-reset p-0 link-hover link-hover-underline">
+      <a href="/about" class="nav-link text-reset p-0 link-hover link-hover-underline">
          <b class="fw-medium">about us</b>
       </a>
    </li>
    <li class="nav-item">
-      <a href="#" class="nav-link text-reset p-0 link-hover link-hover-underline">
+      <a href="/history" class="nav-link text-reset p-0 link-hover link-hover-underline">
          <b class="fw-medium">history</b>
       </a>
    </li>
    <li class="nav-item">
-      <a href="#" class="nav-link text-reset p-0 link-hover link-hover-underline">
+      <a href="/faq" class="nav-link text-reset p-0 link-hover link-hover-underline">
          <b class="fw-medium">FAQ</b>
       </a>
    </li>
    <li class="nav-item">
-      <a href="#" class="nav-link text-reset p-0 link-hover link-hover-underline">
+      <a href="/editorial-policy" class="nav-link text-reset p-0 link-hover link-hover-underline">
          <b class="fw-medium">editorial policy</b>
       </a>
    </li>
    <li class="nav-item">
-      <a href="#" class="nav-link text-reset p-0 link-hover link-hover-underline">
+      <a href="/privacy-policy" class="nav-link text-reset p-0 link-hover link-hover-underline">
          <b class="fw-medium">privacy policy</b>
       </a>
    </li>
+    <li class="nav-item">
+      <a href="/terms" class="nav-link text-reset p-0 link-hover link-hover-underline">
+         <b class="fw-medium">terms</b>
+      </a>
+   </li>
    <li class="nav-item">
-      <a href="#" class="nav-link text-reset p-0 link-hover link-hover-underline">
+      <a href="/contact" class="nav-link text-reset p-0 link-hover link-hover-underline">
          <b class="fw-medium">contact us</b>
       </a>
    </li>
