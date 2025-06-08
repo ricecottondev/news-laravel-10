13263a55d200c709f4df93452dbfe4846d1ddf44
alimasudd <alimasudd.dev@gmail.com>
2025-06-08 18:51:51 +0700

    slicing news 4-category

---
+++ resources/views/front/layouts/news4-category.blade.php
@@ -0,0 +1,199 @@
+<div class="row g-0">
+      <div class="col col-12 col-md-8">
+         <div class="row g-0">
+            <div class="col col-12 px-md-3"><hr></div><!-- end col -->
+            <div class="col col-12 px-3">
+               <h5 class="text-uppercase mb-4">
+                  <b class="fw-bold">businesses</b> <i class="fas fa-chevron-right"></i>
+               </h5>
+            </div><!-- end col -->
+            <div class="col col-12 col-md px-3">
+
+               <div class="news-item">
+                  <header>
+                     <div class="ratio ratio-4x3 news-img mb-3">
+                        <img src="" class="object-fit-cover" alt="">
+                     </div>
+                  </header>
+                  <main>
+                     <h5 class="news-title text-danger fs-5">
+                        <b class="fw-bold">
+                           <a href="detail.html" class="text-reset link-hover-underline">
+                              Sorry, Donald, but the celebrities you covet will never be your friends
+                           </a>
+                        </b>
+                     </h5>
+                     <p class="news-text elipsis-4">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel nostrum voluptates similique libero corrupti maiores incidunt nulla sit ea magni velit, dolorem veritatis delectus non natus voluptatibus molestiae, iure repellat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, iusto facere? Autem voluptates tempore obcaecati quas vel nesciunt placeat debitis perspiciatis incidunt, iusto aliquam architecto dicta? Quas sequi voluptatibus aperiam.
+                     </p>
+                     <div class="news-time media small">
+                        <div class="media-header">
+                           <div class="ratio ratio-1x1 rounded-circle border border-2 border-danger" style="width: 2rem;">
+                              <img src="/assets/template3/asset/img/user/clara.jpg" class="object-fit-cover" alt="">
+                           </div>
+                        </div>
+                        <div class="media-body">
+                           <div><small class="opacity-75">Author by</small> <b class="fw-medium text-danger">Clara</b></div>
+                           <div><small>5 minutes ago</small></div>
+                        </div>
+                     </div>
+                  </main>
+               </div><!-- end news item -->
+
+            </div><!-- end col -->
+            <div class="col col-12 col-md-auto">
+               <hr class="d-md-none">
+               <div class="vr h-100 d-none d-md-block"></div>
+            </div><!-- end col -->
+            <div class="col col-12 col-md">
+               <div class="px-3">
+
+                  <div class="news-item">
+                     <div class="row">
+                        <div class="col col-8">
+                           <h5 class="news-title text-warning fs-5">
+                              <b class="fw-bold">
+                                 <a href="detail.html" class="text-reset link-hover-underline">
+                                    Sorry, Donald, but the celebrities you covet will never be your friends
+                                 </a>
+                              </b>
+                           </h5>
+                           <div class="news-time media small">
+                              <div class="media-header">
+                                 <div class="ratio ratio-1x1 rounded-circle border border-2 border-warning" style="width: 2rem;">
+                                    <img src="/assets/template3/asset/img/user/lola.jpg" class="object-fit-cover" alt="">
+                                 </div>
+                              </div>
+                              <div class="media-body">
+                                 <div><small class="opacity-75">Author by</small> <b class="fw-medium text-danger">Clara</b></div>
+                                 <div><small>5 minutes ago</small></div>
+                              </div>
+                           </div>
+                        </div><!-- end col -->
+                        <div class="col col-4">
+                           <div class="ratio ratio-1x1 news-img">
+                              <img src="" class="object-fit-cover" alt="">
+                           </div>
+                        </div><!-- end col -->
+                     </div><!-- end row -->
+                  </div><!-- end news item -->
+
+               </div>
+               <div class="px-md-3"><hr></div>
+               <div class="px-3">
+
+                  <div class="news-item">
+                     <div class="row">
+                        <div class="col col-8">
+                           <h5 class="news-title text-warning fs-5">
+                              <b class="fw-bold">
+                                 <a href="detail.html" class="text-reset link-hover-underline">
+                                    Sorry, Donald, but the celebrities you covet will never be your friends
+                                 </a>
+                              </b>
+                           </h5>
+                           <div class="news-time media small">
+                              <div class="media-header">
+                                 <div class="ratio ratio-1x1 rounded-circle border border-2 border-warning" style="width: 2rem;">
+                                    <img src="/assets/template3/asset/img/user/lola.jpg" class="object-fit-cover" alt="">
+                                 </div>
+                              </div>
+                              <div class="media-body">
+                                 <div><small class="opacity-75">Author by</small> <b class="fw-medium text-danger">Clara</b></div>
+                                 <div><small>5 minutes ago</small></div>
+                              </div>
+                           </div>
+                        </div><!-- end col -->
+                        <div class="col col-4">
+                           <div class="ratio ratio-1x1 news-img">
+                              <img src="" class="object-fit-cover" alt="">
+                           </div>
+                        </div><!-- end col -->
+                     </div><!-- end row -->
+                  </div><!-- end news item -->
+
+               </div>
+               <div class="px-md-3"><hr></div>
+               <div class="px-3">
+
+                  <div class="news-item">
+                     <div class="row">
+                        <div class="col col-8">
+                           <h5 class="news-title text-warning fs-5">
+                              <b class="fw-bold">
+                                 <a href="detail.html" class="text-reset link-hover-underline">
+                                    Sorry, Donald, but the celebrities you covet will never be your friends
+                                 </a>
+                              </b>
+                           </h5>
+                           <div class="news-time media small">
+                              <div class="media-header">
+                                 <div class="ratio ratio-1x1 rounded-circle border border-2 border-warning" style="width: 2rem;">
+                                    <img src="/assets/template3/asset/img/user/lola.jpg" class="object-fit-cover" alt="">
+                                 </div>
+                              </div>
+                              <div class="media-body">
+                                 <div><small class="opacity-75">Author by</small> <b class="fw-medium text-danger">Clara</b></div>
+                                 <div><small>5 minutes ago</small></div>
+                              </div>
+                           </div>
+                        </div><!-- end col -->
+                        <div class="col col-4">
+                           <div class="ratio ratio-1x1 news-img">
+                              <img src="" class="object-fit-cover" alt="">
+                           </div>
+                        </div><!-- end col -->
+                     </div><!-- end row -->
+                  </div><!-- end news item -->
+
+               </div>
+            </div><!-- end col -->
+         </div><!-- end row -->
+      </div><!-- end col -->
+      <div class="col col-12 col-md-auto">
+         <div class="vr h-100 d-none d-md-block mx-lg-3"></div>
+      </div><!-- end col -->
+      <div class="col col-12 col-md">
+         <div class="sidenav">
+            <div class="px-md-3">
+               <hr class="mb-0">
+            </div>
+            <div class="more px-3">
+               <header>
+                  <h5 class="fs-reset mb-3 text-danger">
+                     <b class="fw-bold">MORE IN BUSINESS</b>
+                  </h5>
+               </header>
+               <main>
+                  <ul class="more-list list-unstyled d-flex flex-column row-gap-4 small">
+                     <li class="more-item">
+                        <a href="" class="text-reset link-hover link-hover-underline">
+                           <b class="fw-bold text-uppercase">soccer</b> - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum vel rem quam commodi necessitatibus, eum, incidunt maxime, tempore odit minima corporis? Laborum, corporis ducimus perspiciatis omnis nemo sapiente nostrum velit.
+                        </a>
+                     </li>
+                     <li class="more-item">
+                        <a href="" class="text-reset link-hover link-hover-underline">
+                           <b class="fw-bold text-uppercase">soccer</b> - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum vel rem quam commodi necessitatibus, eum, incidunt maxime, tempore odit minima corporis? Laborum, corporis ducimus perspiciatis omnis nemo sapiente nostrum velit.
+                        </a>
+                     </li>
+                     <li class="more-item">
+                        <a href="" class="text-reset link-hover link-hover-underline">
+                           <b class="fw-bold text-uppercase">soccer</b> - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum vel rem quam commodi necessitatibus, eum, incidunt maxime, tempore odit minima corporis? Laborum, corporis ducimus perspiciatis omnis nemo sapiente nostrum velit.
+                        </a>
+                     </li>
+                     <li class="more-item">
+                        <a href="" class="text-reset link-hover link-hover-underline">
+                           <b class="fw-bold text-uppercase">soccer</b> - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum vel rem quam commodi necessitatibus, eum, incidunt maxime, tempore odit minima corporis? Laborum, corporis ducimus perspiciatis omnis nemo sapiente nostrum velit.
+                        </a>
+                     </li>
+                     <li class="more-item">
+                        <a href="" class="text-reset link-hover link-hover-underline">
+                           <b class="fw-bold text-uppercase">soccer</b> - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum vel rem quam commodi necessitatibus, eum, incidunt maxime, tempore odit minima corporis? Laborum, corporis ducimus perspiciatis omnis nemo sapiente nostrum velit.
+                        </a>
+                     </li>
+                  </ul>
+               </main>
+            </div><!-- end more -->
+         </div><!-- end sidenav -->
+      </div><!-- end col -->
+   </div><!-- end row -->
