55ca48549c030685917fdc4653f9a28fa3c9828c
alimasudd <alimasudd.dev@gmail.com>
2025-06-18 15:48:31 +0700

    modified detail news

--- resources/views/front/news-detail.blade.php
+++ resources/views/front/news-detail.blade.php
@@ -182,7 +182,7 @@
 								</div>
 							</div>
 
-							<div>
+							{{-- <div>
 								<h5 class="mb-3">
 									<b class="fw-bold">
 										Oi, be honest—what'd you reckon?
@@ -277,9 +277,9 @@
 								<button class="btn btn-primary px-4" id="triggerModalRating" data-bs-toggle="modal" data-bs-target="#modalRating">
 									Send <i class="fas fa-paper-plane"></i>
 								</button>
-							</div>
+							</div> --}}
 
-							<div>
+							{{-- <div>
 								<h5>
 									<b class="fw-bold">
 										Quick Poll or Emoji Slider
@@ -305,7 +305,7 @@
 								<button class="btn btn-primary px-4">
 									Send <i class="fas fa-paper-plane"></i>
 								</button>
-							</div>
+							</div> --}}
 
 							<div>
 								<h5>
